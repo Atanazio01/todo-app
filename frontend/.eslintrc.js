2cc4430f411efc682ef0f503539a836b840771f9
Atanazio01 <marcosd.atanazio@gmail.com>
2021-09-21 22:39:15 -0300

    Criando Componentes Todo, About e Menu

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
     'no-unused-vars': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
+    'react/prefer-stateless-function': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
   },
 };
