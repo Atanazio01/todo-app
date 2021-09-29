3fe3d6b018037355e8a2838695563b2574928b0d
Atanazio01 <marcosd.atanazio@gmail.com>
2021-09-28 21:33:41 -0300

    Pesquisa de TODOs

--- frontend/src/todo/Todo.jsx
+++ frontend/src/todo/Todo.jsx
@@ -18,6 +18,7 @@ export default class Todo extends Component {
     this.handleRemove = this.handleRemove.bind(this);
     this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
     this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
 
     this.refresh();
   }
@@ -34,22 +35,27 @@ export default class Todo extends Component {
 
   handleRemove(todo) {
     axios.delete(`${URL}/${todo._id}`)
-      .then((resp) => this.refresh());
+      .then((resp) => this.refresh(this.state.description));
   }
 
   handleMarkAsDone(todo) {
     axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
-      .then((resp) => this.refresh());
+      .then((resp) => this.refresh(this.state.description));
   }
 
   handleMarkAsPending(todo) {
     axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
-      .then((resp) => this.refresh());
+      .then((resp) => this.refresh(this.state.description));
   }
 
-  refresh() {
-    axios.get(`${URL}?sort=-createdAt`)
-      .then((resp) => this.setState({ ...this.state, description: '', list: resp.data }));
+  handleSearch() {
+    this.refresh(this.state.description);
+  }
+
+  refresh(description = '') {
+    const search = description ? `&description__regex=/${description}/` : '';
+    axios.get(`${URL}?sort=-createdAt${search}`)
+      .then((resp) => this.setState({ ...this.state, description, list: resp.data }));
   }
 
   render() {
@@ -60,6 +66,7 @@ export default class Todo extends Component {
           description={this.state.description}
           handleChange={this.handleChange}
           handleAdd={this.handleAdd}
+          handleSearch={this.handleSearch}
         />
         <TodoList
           list={this.state.list}
